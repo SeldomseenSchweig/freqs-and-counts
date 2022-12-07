ff2352878e760e329795c637ce06932678c44fc6
SeldomseenSchweig <jamesschweig@gmail.com>
2022-12-07 13:20:37 -0800

    the first one is mine, tne second one is from the solutions, mine works but the time complexity is noy good

--- count-pairs.js
+++ count-pairs.js
@@ -1,26 +1,45 @@
 // add whatever parameters you deem necessary
-function countPairs(arr, goal) {
+function countPairs(arr, num) {
 
 
-  let i = 0;
-  let diffs = []
+//   let i = 0;
+//   let diffs = []
 
-while (i < arr.length) {
-  diffs.push(goal-arr[i])
-  i++
-}
-let j = 0
-let count = 0 
-while(j < arr.length){
-  if(arr.includes(diffs[j]) && j != arr.indexOf(diffs[j])){
-    count++
-    j++
-  }else{
-    j++
+// while (i < arr.length) {
+//   diffs.push(goal-arr[i])
+//   i++
+// }
+// let j = 0
+// let count = 0 
+// while(j < arr.length){
+//   if(arr.includes(diffs[j]) && j != arr.indexOf(diffs[j])){
+//     count++
+//     j++
+//   }else{
+//     j++
+//   }
+// }
+// let answer = Math.floor(count/2)
+//   return answer;
+
+
+  arr.sort((a, b) => a - b);
+  let count = 0;
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+      let sum = arr[start] + arr[end];
+      if (sum === num) {
+          count++;
+          start++;
+          end--;
+      } else if (sum < num) {
+          start++;
+      } else {
+          end--;
+      }
   }
-}
-let answer = Math.floor(count/2)
-  return answer;
+  return count;
 }
 
 
